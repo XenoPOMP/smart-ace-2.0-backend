dd6b209f1f66316d5afd265213947db9a5e59286
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-11-10 21:09:06 +0300

    generate prisma schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MysqlModule } from 'nest-mysql';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-	imports: [
-		// MysqlModule.forRootAsync({
-		// 	useFactory: () => ({
-		// 		host: process.env.MYSQL_HOST,
-		// 		database: process.env.MYSQL_DATABASE,
-		// 		user: process.env.MYSQL_USER,
-		// 		password: process.env.MYSQL_PASSWORD,
-		// 		port: parseInt(process.env.MYSQL_PORT),
-		// 	}),
-		// }),
-	],
+	imports: [],
 	controllers: [AppController],
 	providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,7 @@
 import { ConsoleLogger, Injectable } from '@nestjs/common';
-import { Connection } from 'mysql2';
-import { InjectClient } from 'nest-mysql';
 
 @Injectable()
 export class AppService {
-	// constructor(@InjectClient() private readonly connection: Connection) {}
-
 	private logger: ConsoleLogger = new ConsoleLogger();
 
 	async getHello(): Promise<string> {
